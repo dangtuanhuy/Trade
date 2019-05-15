0a6d373daec645065401f9aa529630408a38994a
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-15 15:55:10 +0700

    save

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ import App from './App'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
+
